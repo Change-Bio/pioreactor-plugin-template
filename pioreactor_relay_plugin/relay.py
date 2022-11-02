225320b63594b5c4af38272a9479e50aa873c16e
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-11-02 10:51:02 -0400

    on -> relay_on since yaml treats 'on' as a boolean, converting it to true

--- pioreactor_relay_plugin/relay.py
+++ pioreactor_relay_plugin/relay.py
@@ -12,7 +12,7 @@ from pioreactor.whoami import get_unit_name
 class Relay(BackgroundJobWithDodgingContrib):
 
     published_settings = {
-        "on": {"datatype": "boolean", "settable": True},
+        "relay_on": {"datatype": "boolean", "settable": True},
     }
 
     job_name = "relay"
@@ -21,10 +21,10 @@ class Relay(BackgroundJobWithDodgingContrib):
         super().__init__(unit=unit, experiment=experiment, plugin_name="relay")
         if start_on:
             self.duty_cycle = 100
-            self.on = True
+            self.relay_on = True
         else:
             self.duty_cycle = 0
-            self.on = False
+            self.relay_on = False
 
         self.pwm_pin = PWM_TO_PIN[config.get("PWM_reverse", "relay")]
         # looks at config.ini/configuration on UI to match
@@ -36,39 +36,39 @@ class Relay(BackgroundJobWithDodgingContrib):
         )  # since we also go 100% high or 0% low, we don't need hz.
         self.pwm.lock()
 
-    def set_on(self, value: bool):
-        if value == self.on:
+    def set_relay_on(self, value: bool):
+        if value == self.relay_on:
             return
         if value:
             self.set_duty_cycle(100)
-            self.on = True
+            self.relay_on = True
         else:
             self.set_duty_cycle(0)
-            self.on = False
+            self.relay_on = False
 
     def set_duty_cycle(self, new_duty_cycle: float):
         self.duty_cycle = new_duty_cycle
         self.pwm.change_duty_cycle(self.duty_cycle)
 
     def on_init_to_ready(self):
-        self.logger.debug(f"Starting relay {'ON' if self.on else 'OFF'}.")
+        self.logger.debug(f"Starting relay {'ON' if self.relay_on else 'OFF'}.")
         self.pwm.start(self.duty_cycle)
 
     def on_ready_to_sleeping(self):
-        self.set_on(False)
+        self.set_relay_on(False)
 
     def on_sleeping_to_ready(self):
-        self.set_on(True)
+        self.set_relay_on(True)
 
     def on_disconnected(self):
-        self.set_on(False)
+        self.set_relay_on(False)
         self.pwm.cleanup()
 
     def action_to_do_before_od_reading(self):
-        self.set_on(False)
+        self.set_relay_on(False)
 
     def action_to_do_after_od_reading(self):
-        self.set_on(True)
+        self.set_relay_on(True)
 
 
 import click
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
 
 
 setup(
     name="pioreactor_relay_plugin",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     description="Turn the PWM channels into a simple on/off relay for additional hardware.",
     long_description=open("README.md").read(),
@@ -14,7 +17,5 @@ setup(
     url="https://github.com/CamDavidsonPilon/pioreactor-relay-plugin",
     packages=find_packages(),
     include_package_data=True,
-    entry_points={
-        "pioreactor.plugins": "pioreactor_relay_plugin = pioreactor_relay_plugin"
-    },
+    entry_points={"pioreactor.plugins": "pioreactor_relay_plugin = pioreactor_relay_plugin"},
 )
